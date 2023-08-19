6e47485149cb8dac673b640109ddf28467ca47c7
Mikhail <shaltynovm@gmail.com>
2023-08-19 15:28:06 +0530

    initial commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,5 +5,21 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        for (String s : args) {
+            if (s == null) {
+                return false;
+            }
+            if (s.startsWith("0") && s.length() > 1) {
+                return false;
+            }
+            try {
+                if (Double.parseDouble(s) <= 0) {
+                    return false;
+                }
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,22 @@
 package com.epam.utils;
 
 public class StringUtils {
+
     public static boolean isPositiveNumber(String str) {
         //code!
+        if (str == null) {
+            return false;
+        }
+        if (str.startsWith("0")&&str.length()>1) {
+            return false;
+        }
+        try {
+            if (Double.parseDouble(str) <= 0) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
